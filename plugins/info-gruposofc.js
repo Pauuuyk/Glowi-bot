c32b1f0c0ae515d68a38fd478861724eb5929089
boybenjx7 <josebenjamin202444@gmail.com>
2024-05-13 14:08:25 +1300

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,9 +1,9 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*HYPERV TE INVITA A UNIRTE AL GRUPO OFICIAL DE WHATSAPP*
+  const text = `*ＨｙｐｅｒＶ TE INVITA A UNIRTE AL GRUPO OFICIAL DE WHATSAPP*
 
-*➤ 𝙶𝚛𝚞𝚙𝚘 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝙱𝚘𝚝:*
+*➤ 𝙶𝚛𝚞𝚙𝚘 𝚘𝚏𝚒𝚌𝚒𝚊𝚕 𝚍𝚎𝚕 𝙱𝚘𝚝 Hyper V:*
 *1.-* https://chat.whatsapp.com/DXAdja6QqGsEqMtuTJNTXn`.trim();
   const buttonMessage= {
     'document': {url: `https://www.instagram.com/boybenjx7`},
@@ -17,9 +17,9 @@ const handler = async (m, {conn, usedPrefix}) => {
         'mediaUrl': 'https://www.instagram.com/boybenjx7',
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': 'HYPERV BOT : @BOYBENJX7',
+        'title': 'ＨｙｐｅｒＶ ／ Ｃｒｅａｄｏ ｐｏｒ ＠ｂｏｙｂｅｎｊｘ７',
         'body': wm,
-        'thumbnail': imagen1,
+        'thumbnail': 'https://th.bing.com/th/id/OIG2.t6SMLw75Pp9ssDkcg_m7?pid=ImgGn',
         'sourceUrl': 'https://www.instagram.com/boybenjx7'}},
     'caption': text,
     'footer': wm,
