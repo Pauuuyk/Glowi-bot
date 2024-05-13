417f619b3458aae88a50bf2dacc435e5a788c065
boybenjx7 <josebenjamin202444@gmail.com>
2024-05-14 05:02:26 +1300

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,27 +1,27 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴𝙻 𝙱𝙾𝚃*
+  const text = `*𝐻𝓎𝓅𝑒𝓇𝒱 𝐵𝑜𝓉 𝓉𝑒 𝒾𝓃𝓋𝒾𝓉𝒶 𝒶 𝓊𝓃𝒾𝓇𝓉𝑒 𝒶𝓁 𝑔𝓇𝓊𝓅𝑜 𝑜𝒻𝒾𝒸𝒾𝒶𝓁 𝒹𝑒 𝒲𝒽𝒶𝓉𝓈𝒜𝓅𝓅*
 
-*➤ 𝙶𝚛𝚞𝚙𝚘 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝙱𝚘𝚝:*
-*1.-* https://chat.whatsapp.com/KtyGuKlic5w9xlw8DJlSFD`.trim();
+*➤ 𝒢𝓇𝓊𝓅𝑜 𝑜𝒻𝒾𝒸𝒾𝒶𝓁 𝒹𝑒 𝐻𝓎𝓅𝑒𝓇𝒱 𝐵𝑜𝓉:*
+*1.-* https://chat.whatsapp.com/DXAdja6QqGsEqMtuTJNTXn`.trim();
   const buttonMessage= {
-    'document': {url: `https://www.instagram.com/ceogeripium.dzn`},
+    'document': {url: `https://www.instagram.com/boybenjx7`},
     'mimetype': `application/${document}`,
-    'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
+    'fileName': `「  𝓞𝔀𝓷𝓮𝓻 @𝓫𝓸𝔂𝓫𝓮𝓷𝓳𝔁7 」`,
     'fileLength': 99999999999999,
     'pageCount': 200,
     'contextInfo': {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://www.instagram.com/ceogeripium.dzn',
+        'mediaUrl': 'https://www.instagram.com/boybenjx7',
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
+        'title': '𝐻𝓎𝓅𝑒𝓇𝒱 𝑒𝓁 𝓂𝑒𝒿𝑜𝓉 𝐵𝑜𝓉 𝒹𝑒 𝒲𝒽𝒶𝓉𝓈𝒜𝓅𝓅⁩',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.instagram.com/ceogeripium.dzn'}},
+        'sourceUrl': 'https://www.instagram.com/boybenjx7'}},
     'caption': text,
     'footer': wm,
     'headerType': 6};
