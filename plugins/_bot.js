25338198fa46c46192104c575d89742d6635ea86
boybenjx7 <josebenjamin202444@gmail.com>
2024-05-20 18:35:24 +1300

    Update and rename _aaa to _bot.js

---
+++ plugins/_bot.js
@@ -0,0 +1,13 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Fem16);
+};
+handler.command = /^(bot|bot)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.bot = ` 
+*[𝐇𝐘𝐏𝐄𝐑´𝐕 𝐁𝐎𝐓]*
+`;
+
